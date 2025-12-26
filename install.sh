915a1a8c688a8a9e7379fdace45e333512e6a9ad
totallycrazy <u1305525087@gmail.com>
2025-12-26 10:31:33 +0000

    jhv

--- install.sh
+++ install.sh
@@ -2,7 +2,12 @@
 set -euo pipefail
 
 DOTFILES="$HOME/dotfiles"
+
 git pull
+
+if ! echo "$PATH" | grep -q "$HOME/.local/bin"; then
+  echo 'export PATH="$HOME/.local/bin:$PATH"' >> "$HOME/.bashrc"
+fi
 
 ask_on_conflict() {
   local dst="$1"
@@ -58,32 +63,24 @@ link() {
   [[ -x "$src" ]] && chmod u+x "$dst"
 }
 
-link bash/bashrc bashrc
-link bash/profile bash_profile
-link bash/aliases bash_aliases
-link git/gitconfig gitconfig
-
 echo "Installing packages…"
 sudo apt update
 sudo apt install -y \
   curl wget git magic-wormhole fd-find sd nnn micro broot \
   lazygit trippy xh yq duf gdu du-dust lnav procs gping \
-  fzf zoxide ripgrep bat lsd eza dtrx
+  fzf zoxide ripgrep bat lsd eza dtrx translate-shell \
+  most npm
+
+npm install -g @microsoft/inshellisense
+
 
 # fd-find / bat Debian-Namenschaos fixen
 command -v fdfind >/dev/null && ln -sf "$(command -v fdfind)" "$HOME/.local/bin/fd"
 command -v batcat >/dev/null && ln -sf "$(command -v batcat)" "$HOME/.local/bin/bat"
 
-# sicherstellen, dass ~/.local/bin im PATH ist
-if ! echo "$PATH" | grep -q "$HOME/.local/bin"; then
-  echo 'export PATH="$HOME/.local/bin:$PATH"' >> "$HOME/.bashrc"
-fi
-
 link bash/bashrc bashrc
 link bash/profile bash_profile
 link bash/aliases bash_aliases
 link git/gitconfig gitc
-
-git clone https://github.com/marlonrichert/bash-autocomplete.git ~/.bash-autocomplete
 
 . "$HOME/.bashrc"
