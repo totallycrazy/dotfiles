555c52de15ef11521b407b71f4a11ccf1eff9c6c
totallycrazy <u1305525087@gmail.com>
2025-12-26 03:32:34 +0000

    chmod

--- install.sh
+++ install.sh
@@ -9,19 +9,21 @@ link() {
 
   if [ -e "$dst" ] && [ ! -L "$dst" ]; then
     echo "⚠️  $dst exists – skipping, but setting execution rights..."
-    chmod u+x "$dst"
-    cd ~ 
-    source .bashrc
+    if [ -e "$dst" ] ; then
+      chmod u+x "$dst"
+    fi
     return
   fi
 
   ln -sf "$src" "$dst"
-  chmod u+x "$dst"
   echo "✓ $dst"
+  if [ -e "$dst" ] ; then
+    chmod u+x "$dst"
+  fi
 }
 
 link bash/bashrc bashrc
-link bash/bash_profile bash_profile
+link bash/profile bash_profile
 link git/gitconfig gitconfig
 
 sudo apt install -y curl wget git magic-wormhole fd-find sd nnn micro broot lazygit trippy xh yq duf gdu du-dust lnav procs gping fzf zoxide ripgrep bat lsd eza dtrx
